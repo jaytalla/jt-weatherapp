bd17c1ef953860bf2c0ac746e0231fabd2ee51fb
jayraytalla <jayraytallaworkacc@gmail.com>
2024-03-18 08:28:12 +0800

    First Update for Weather App

---
+++ src/sections/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center items-center w-full  h-screen bg-primary-color'>
+        <div className='w-[40%] h-[60%] bg-white rounded-xl'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
